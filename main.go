8d29bedde39aa86471ed1b0ac92191ce63ef1c98
Mehran <janfeshan.mehran@gmail.com>
2018-05-08 20:50:34 +0800

    Added new functions

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ package main
 func main() {
 	// cards := newDeck()
 	// cards.saveToFile("myfile123")
-	cards := newDeckFromFile("mayfile123")
+	// cards := newDeckFromFile("mayfile124")
+	// cards.print()
+
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
 }
